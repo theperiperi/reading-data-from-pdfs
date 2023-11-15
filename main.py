82382cc346962761e41ce7f49075a319a062aebf
Mahas1 <mahasvan@gmail.com>
2023-11-15 14:46:57 +0530

    add middleware

--- main.py
+++ main.py
@@ -3,10 +3,20 @@ from typing import Annotated
 from fastapi import FastAPI
 from fastapi.params import File
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
+
 
 from pdf_reader import PDFTextExtractor
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
